c9613fde8d4948facc085e59570dcc5c36053aef
mosorio22 <moso4410@gmail.com>
2022-04-18 21:21:58 -0400

    add hero image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  
+
 }
 
 export default App;
---
+++ src/HeroImage/HeroImage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HeroImage.css';
+
+function HeroImage(props) {
+    return (
+        <div
+            class='hero-image'
+        >
+            <div class="hero-text">
+                <h1>Wanderlust</h1>
+                <p>Adventure Awaits</p>
+            </div>
+        </div>
+    );
+}
+
+export default HeroImage;
