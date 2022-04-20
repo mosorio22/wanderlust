e1fed5b0b597357ba25b55143f9b2591750a78d1
mosorio22 <moso4410@gmail.com>
2022-04-20 14:17:24 -0400

    import apiKeys

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {apiKeys} from './api_store.js';
 
 //import App from './App';
 import HeroImage from './HeroImage/HeroImage'
