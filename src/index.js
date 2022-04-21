978eb3a97c25b6e5664c04c4474abc81a2cc8542
mosorio22 <moso4410@gmail.com>
2022-04-20 22:03:17 -0400

    switch hero header to div and place in existing header

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import PlaceholderImage from './PlaceholderImage/PlaceholderImage'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HeroImage />
+  	<header className="header-section">
+  		<HeroImage />
+  	</header>
     <main className="main-section">
     	<PlaceholderImage />
     </main>
