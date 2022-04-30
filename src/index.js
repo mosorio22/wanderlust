9648c37d5c5c67d893eb03e792cf75cfa98c0616
mosorio22 <moso4410@gmail.com>
2022-04-29 21:37:35 -0400

    get hiking data from api. get lat and long from submitted zip code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {apiKeys} from './api_store.js';
 
 //components
 import HeroImage from './HeroImage/HeroImage'
